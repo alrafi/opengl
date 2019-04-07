e4cf5fbdf01b4c83755720f00165fc90a1c9177f
Kurniandha Sukma Yunastrian <13516106@std.stei.itb.ac.id>
2019-04-08 05:20:27 +0700

    fix bug

--- Task3-Jeep/jeep.cpp
+++ Task3-Jeep/jeep.cpp
@@ -48,7 +48,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Jeep Puyeng", NULL, NULL);
     if (window == NULL)
     {
         //std::cout << "Failed to create GLFW window" << std::endl;
